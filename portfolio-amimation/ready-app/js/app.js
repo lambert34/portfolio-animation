52ab9082fe794b627c635ba6c07d0aa981df8908
lambert34 <aleksandrlambert@gmail.com>
2023-05-01 18:09:59 +0700

    оптимизировали итерацию ++ и приподняли слайды

--- portfolio-amimation/ready-app/js/app.js
+++ portfolio-amimation/ready-app/js/app.js
@@ -1,11 +1,12 @@
 
 
 document.querySelectorAll('.slider').forEach((n, i) => {
-    window['slider${i+1}'] = new Swiper(n, {
+    window['slider${i++}'] = new Swiper(n, {
         freeMode: true,
         centeredSlides: true,
         direction: 'vertical',
         mousewheel: true,
         slidesPerView: 1.75,
+        slidesOffsetBefore: -125
     });
 })
