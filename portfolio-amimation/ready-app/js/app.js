1e58359e0596fd418f9b3075ab13bb4e25e27c0f
lambert34 <aleksandrlambert@gmail.com>
2023-05-01 19:00:33 +0700

    фиксит баг (со скобками повнимательнее)

--- portfolio-amimation/ready-app/js/app.js
+++ portfolio-amimation/ready-app/js/app.js
@@ -1,5 +1,5 @@
 document.querySelectorAll('.slider').forEach((n, i) => {
-    window['slider${i+1}'] = new Swiper(n, {
+    window[`slider${i+1}`] = new Swiper(n, {
         freeMode: true,
         centeredSlides: true,
         direction: 'vertical',
